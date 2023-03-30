712878530cfe1f2508820eb3a9005dee0e5173ec
AbhishekSinghT <abhisheksingh.luminoguru@gmail.com>
2023-03-30 14:16:01 +0530

    first commit 11

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,7 +8,6 @@ export default function Document() {
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
-          crossOrigin="true" 
         />
         <link
           href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,400;0,600;0,700;0,900;1,400&display=swap"
